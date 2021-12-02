20fda2a8475493222cd712db233c29da84d544ae
akurkcu <kurkcuabdurrahman@hotmail.com>
2021-12-02 15:39:57 +0300

    2 Aralık Çalışma Sonu

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// console.log("Hello")
+
+// let arabaKredileri = ["Sıfır Araç Kredisi", "2.el Araç kredisi" , "Motorsiklet Kredisi"]
+
+// for (let i = 0; i < arabaKredileri.length; i++) {
+//     console.log(arabaKredileri[i]);
+    
+// }
+
